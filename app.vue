79c3595e179f93095a75f804123f5a62a4f53fa7
Your Name <you@example.com>
2023-01-27 10:48:44 +0400

    secound

--- app.vue
+++ app.vue
@@ -10,9 +10,9 @@
 </template>
 
 <script setup>
-// const { $socket } = useNuxtApp();
-import io from "socket.io-client";
 
+import io from "socket.io-client";
+ const { $socket } = useNuxtApp();
 const message = ref("");
 const text = ref("");
 let socket;
@@ -20,12 +20,35 @@ function send() {
   console.log(text.value);
   socket.emit("message", text.value);
 }
+
+
+
 onMounted(() => {
-  socket = io("http://192.168.20.31:3000");
+  socket = io("http://localhost:3000/");
 
   socket.on("message", (data) => {
-    console.log(data, "<<<<");
+    console.log(data.nickname+ "<<<<");
     message.value = data;
   });
+
+  //level 1
+  socket.on('user connected', (data) => {
+    console.log(data.message);
+  });
+  socket.on('user disconnected', (data) => {
+    console.log(data.message);
+  });
+
+  //level2
+  socket.on('new message', (data) => {
+    console.log(`${data.nickname}: ${data.message}`);
+  });
+
+  const nickname = prompt('Choose a nickname:');
+  socket.emit('set nickname', nickname);
+  
 });
+
+//level 4
+
 </script>
--- modules/socket.js
+++ modules/socket.js
@@ -1,32 +1,45 @@
 import { Server } from "socket.io";
 import { defineNuxtModule } from "@nuxt/kit";
-
+let connectedUsers = [];
 export default defineNuxtModule({
-  setup(_, nuxt) {
-    nuxt.hook("listen", (server) => {
-      console.log("Socket listen", server.address(), server.eventNames());
-      const io = new Server(server);
+    setup(_, nuxt) {
+        nuxt.hook("listen", (server) => {
+            console.log("Socket listen", server.address(), server.eventNames());
+            const io = new Server(server);
 
-      nuxt.hook("close", () => io.close());
+            nuxt.hook("close", () => io.close());
 
-      io.on("connection", (socket) => {
-        console.log("Connection", socket.id);
-      });
+            io.on("connection", (socket) => {
+                console.log("Connection", socket.nickname);
+                //level 5
+                connectedUsers.push(socket.nickname);
+                io.emit('update users', connectedUsers);
+                //level 1
+                console.log('a user connected');
+                socket.broadcast.emit('user connected', { message: 'A user has connected ' + socket.nickname });
 
-      io.on("connect", (socket) => {
-        socket.emit("message", `welcome ${socket.id}`);
-        socket.broadcast.emit("message", `${socket.id} joined`);
+                socket.on('disconnect', () => {
+                    connectedUsers = connectedUsers.filter(nickname => nickname !== socket.nickname);
+                    io.emit('update users', connectedUsers);
+                    console.log('user disconnected');
+                    socket.broadcast.emit('user disconnected', { message: 'A user has disconnected' });
+                });
 
-        socket.on("message", function message(data) {
-          console.log("message received: %s", data);
-          socket.broadcast.emit("message", data);
+                socket.on('new message', (data) => {
+                    data.nickname = socket.nickname;
+                    socket.broadcast.emit('new message', data);
+                });
+                //level 2
+                socket.on('set nickname', (nickname) => {
+                    socket.nickname = nickname;
+                });
+                //level 4
+                socket.on("typing", (data) => {
+                    socket.broadcast.emit("typing", "user is typing...")
+                })
+            });
+
+
         });
-
-        socket.on("disconnecting", () => {
-          console.log("disconnected", socket.id);
-          socket.broadcast.emit("message", `${socket.id} left`);
-        });
-      });
-    });
-  },
+    },
 });
