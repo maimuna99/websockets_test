136beed9dd204ec0a69fd3b0ce90c2f55163cb92
Your Name <you@example.com>
2023-01-30 07:37:41 +0100

    fifth

--- app.vue
+++ app.vue
@@ -2,50 +2,71 @@
   <div>
     <p>Check socket status in Vue devtools...</p>
     <p>
-      {{ message }}
+      
     </p>
-    <input v-model="text" />
-    <button @click="send">SEND</button>
+    <input v-model="text" @focus="typing" />
+    <button @click="send" >SEND</button>
+  </div>
+  <div>
+  <p>
+    massage:{{ txt }}
+  </p>
   </div>
 </template>
 
 <script setup>
 
 import io from "socket.io-client";
- const { $socket } = useNuxtApp();
+
 const message = ref("");
+const txt = ref("...");
 const text = ref("");
+const flag = ref(true)
+const nickname = prompt('Choose a nickname:');
+
 let socket;
 function send() {
-  console.log(text.value);
-  socket.emit("message", text.value);
+  //console.log(text.value);
+  socket.emit("message", (nickname + text.value));
+}
+function typing(){
+  
+  socket.emit("message", (nickname + ": typing..."));
+}
+function stoptyping(){
+  socket.emit("message", "I am typing...");
+  flag =false;
 }
 
-
-
 onMounted(() => {
-  socket = io("http://localhost:3000/");
+  socket = io("ws://localhost:3000/");
+  socket.nickname= nickname;
+  socket.on("Welcome",(data)=>{ 
+    console.log(data)
+  })
+   socket.on("message", (data) => {
+    console.log(data)
+     txt.value +=  data;
+     
+   });
 
-  socket.on("message", (data) => {
-    console.log(data.nickname+ "<<<<");
-    message.value = data;
-  });
+  // //level 1
+  // socket.on('user connected', (data) => {
+  //   console.log(data.message);
+  // });
+  // socket.on('user disconnected', (data) => {
+  //   console.log(data.message);
+  // });
 
-  //level 1
-  socket.on('user connected', (data) => {
-    console.log(data.message);
-  });
-  socket.on('user disconnected', (data) => {
-    console.log(data.message);
-  });
+  // //level2
+  // socket.on('new message', (data) => {
+  //   txt.value += data.message; 
+  //   console.log('${data.nickname}: ${data.message}');
+  // });
 
-  //level2
-  socket.on('new message', (data) => {
-    console.log(`${data.nickname}: ${data.message}`);
-  });
 
-  const nickname = prompt('Choose a nickname:');
-  socket.emit('set nickname', nickname);
+
+   
   
 });
 
--- modules/socket.js
+++ modules/socket.js
@@ -1,43 +1,24 @@
 import { Server } from "socket.io";
 import { defineNuxtModule } from "@nuxt/kit";
-let connectedUsers = [];
+let usersmap = {};
 export default defineNuxtModule({
     setup(_, nuxt) {
         nuxt.hook("listen", (server) => {
-            console.log("Socket listen", server.address(), server.eventNames());
-            const io = new Server(server);
-
-            nuxt.hook("close", () => io.close());
+            console.log("Socket listen", server.address());
+            const io = new Server(server, {
+                cors: {
+                    origin: "*",
+                },
+            });
 
             io.on("connection", (socket) => {
-                console.log("Connection", socket.nickname);
-                //level 5
-                connectedUsers.push(socket.nickname);
-                io.emit('update users', connectedUsers);
-                //level 1
-                console.log('a user connected');
-                socket.broadcast.emit('user connected', { message: 'A user has connected ' + socket.nickname });
 
-                socket.on('disconnect', () => {
-                    connectedUsers = connectedUsers.filter(nickname => nickname !== socket.nickname);
-                    io.emit('update users', connectedUsers);
-                    console.log('user disconnected');
-                    socket.broadcast.emit('user disconnected', { message: 'A user has disconnected' });
-                });
-
-                socket.on('new message', (data) => {
-                    data.nickname = socket.nickname;
-                    socket.broadcast.emit('new message', data);
-                });
-                //level 2
-                socket.on('set nickname', (nickname) => {
-                    socket.nickname = nickname;
-                });
-                //level 4
-                socket.on("typing", (data) => {
-                    socket.broadcast.emit("typing", "user is typing...")
+                socket.emit("Welcome", "Welcome to the server")
+                socket.on("message", (data) => {
+                    socket.broadcast.emit("message", socket.nickname + ":" + data);
                 })
-            });
+            })
+
 
 
         });
